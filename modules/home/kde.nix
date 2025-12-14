011535ccbf9d372400e3dbb1bf23d7e2e4f0ec12
Matthieu <greep@greep.fr>
2025-12-14 20:07:32 +0100

    Added top panel

--- modules/home/kde.nix
+++ modules/home/kde.nix
@@ -146,11 +146,93 @@
               };
             };
           }
+        ];
+      }
+      {
+        location = "top";
+        height = 26;
+        widgets = [
+          {
+            applicationTitleBar = {
+              behavior = {
+                activeTaskSource = "activeTask";
+                filterByActivity = true;
+                filterByScreen = true;
+                filterByVirtualDesktop = true;
+                disableForNotMaximized = false;
+                disableButtonsForNotHovered = false;
+              };
+              layout = {
+                elements = [ "windowIcon" "windowTitle" ];
+                horizontalAlignment = "left";
+                showDisabledElements = "deactivated";
+                verticalAlignment = "center";
+              };
+              overrideForMaximized.enable = false;
+              titleReplacements = [
+                {
+                  type = "regexp";
+                  originalTitle = ''\bDolphin\b'';
+                  newTitle = "File manager";
+                }
+                {
+                  type = "regexp";
+                  originalTitle = ''\bVisual Studio Code.*\b'';
+                  newTitle = "Visual Studio Code";
+                }
+              ];
+              windowTitle = {
+                undefinedWindowTitle = "";
+                font = {
+                  bold = false;
+                  fit = "fixedSize";
+                  size = 10;
+                };
+                hideEmptyTitle = true;
+                margins = {
+                  bottom = 0;
+                  left = 10;
+                  right = 5;
+                  top = 0;
+                };
+                source = "appName";
+              };
+            };
+          }
+          "org.kde.plasma.panelspacer"
+          {
+            plasmusicToolbar = {
+              panelIcon = {
+                albumCover = {
+                  useAsIcon = false;
+                  radius = 8;
+                };
+                icon = "view-media-track";
+              };
+              playbackSource = "auto";
+              musicControls = {
+                showPlaybackControls = true;
+                volumeStep = 5;
+              };
+              songText = {
+                displayInSeparateLines = true;
+                maximumWidth = 640;
+                scrolling = {
+                  behavior = "alwaysScroll";
+                  speed = 3;
+                  resetOnPause = true;
+                };
+              };
+            };
+          }
+          "org.kde.plasma.panelspacer"
           {
             digitalClock = {
               calendar.firstDayOfWeek = "monday";
               time.format = "24h";
               time.showSeconds = "always";
+              date.enable = false;
+              font = null;
             };
           }
         ];
