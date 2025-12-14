e9ed3c31253d769724270ef64fc0eae13d43fc94
Matthieu <greep@greep.fr>
2025-12-14 11:58:59 +0100

    Update vscode desktop button

--- modules/home/kde.nix
+++ modules/home/kde.nix
@@ -76,7 +76,7 @@
                 "applications:firefox.desktop"
                 "applications:org.kde.dolphin.desktop"
                 "applications:org.kde.konsole.desktop"
-                "applications:code.desktop"
+                "applications:code-url-handler.desktop"
               ];
             };
           }
