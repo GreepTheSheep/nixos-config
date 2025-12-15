f514957ca3faa1f3c339dcfb4b5ad621d91f0781
Matthieu <greep@greep.fr>
2025-12-15 11:52:45 +0100

    Refractor KDE Plasma

---
+++ modules/home/kde/general.nix
@@ -0,0 +1,92 @@
+{ lib, pkgs, ... }:
+
+{
+  # Most Plasma configuration is done system-wide in modules/desktop/kde.nix
+  # However, we can add user-specific Plasma settings here if needed.
+
+  # Example: KDE Connect (phone integration)
+  # services.kdeconnect = {
+  #   enable = true;
+  #   indicator = true;
+  # };
+
+  # Example: Theme specific packages meant for KDE
+  home.packages = with pkgs; [
+    papirus-icon-theme
+    # libsForQt5.qt5.qtgraphicaleffects
+  ];
+
+  programs.plasma = {
+    enable = true;
+
+    workspace = {
+      lookAndFeel = lib.mkForce "org.kde.breezedark.desktop";
+      iconTheme = lib.mkForce "Papirus-Dark";
+      wallpaper = "${../../../wallpaper/stolas.png}";
+    };
+
+    kscreenlocker = {
+      lockOnResume = true;
+      timeout = 5;
+      passwordRequired = true;
+      passwordRequiredDelay = 30;
+      lockOnStartup = false;
+      appearance = {
+        alwaysShowClock = true;
+        showMediaControls = true;
+        wallpaper = "${../../../wallpaper/stolas.png}";
+        wallpaperPictureOfTheDay = null;
+        wallpaperSlideShow = null;
+        wallpaperPlainColor = null;
+      };
+    };
+
+    powerdevil = {
+      AC = {
+        powerButtonAction = "lockScreen";
+        whenLaptopLidClosed = "doNothing";
+        autoSuspend = {
+          action = "nothing";
+        };
+        turnOffDisplay = {
+          idleTimeout = 900;
+          idleTimeoutWhenLocked = "immediately";
+        };
+      };
+      battery = {
+        powerButtonAction = "hibernate";
+        whenLaptopLidClosed = "doNothing";
+        turnOffDisplay = {
+          idleTimeout = 120;
+          idleTimeoutWhenLocked = "immediately";
+        };
+        autoSuspend = {
+          action = "sleep";
+          idleTimeout = 300;
+        };
+        whenSleepingEnter = "standbyThenHibernate";
+      };
+      lowBattery = {
+        whenLaptopLidClosed = "hibernate";
+        turnOffDisplay = {
+          idleTimeout = 60;
+          idleTimeoutWhenLocked = "immediately";
+        };
+        autoSuspend = {
+          action = "hibernate";
+          idleTimeout = 120;
+        };
+      };
+    };
+
+    configFile = {
+      kdeglobals."General"."ColorScheme" = "BreezeDark";
+      baloofilerc."Basic Settings"."Indexing-Enabled" = false;
+      kwinrc.Desktops.Number = {
+        value = 1;
+        # Forces kde to not change this value (even through the settings app).
+        immutable = true;
+      };
+    };
+  };
+}
---
+++ modules/home/kde/panels.nix
@@ -0,0 +1,216 @@
+{ lib, pkgs, ... }:
+
+{
+  programs.plasma = {
+    panels = [
+      {
+        location = "bottom";
+        screen = "all";
+        hiding = "dodgewindows";
+        opacity = "translucent";
+        floating = true;
+        height = lib.mkForce 44;
+        widgets = [
+          {
+            panelSpacer = {
+              expanding = true;
+            };
+          }
+          {
+            kickoff = {
+              icon = lib.mkForce "nix-snowflake-white";
+              favoritesDisplayMode = "grid";
+              applicationsDisplayMode = "list";
+              showActionButtonCaptions = false;
+              showButtonsFor = {
+                custom = [
+                  "shutdown"
+                  "reboot"
+                  "logout"
+                ];
+              };
+            };
+          }
+          {
+            iconTasks = {
+              launchers = [
+                "applications:firefox.desktop"
+                "applications:org.kde.dolphin.desktop"
+                "applications:org.kde.konsole.desktop"
+                "applications:code.desktop"
+                #"applications:code-url-handler.desktop"
+              ];
+            };
+          }
+          {
+            panelSpacer = {
+              expanding = true;
+            };
+          }
+          {
+            systemTray.items = {
+              shown = [
+                "org.kde.plasma.networkmanagement"
+                "org.kde.plasma.battery"
+                "org.kde.plasma.bluetooth"
+                "org.kde.plasma.volume"
+              ];
+              # hidden = [];
+            };
+          }
+          {
+            name = "org.kde.plasma.systemmonitor";
+            config = {
+              Appearance = {
+                title = "Processeur";
+                chareFace = "org.kde.ksysguard.piechart";
+                updateRateLimit = "1000";
+              };
+              SensorLabels = {
+                "cpu/all/usage" = "Usage";
+                "cpu/all/averageFrequency" = "Frequence moyenne";
+                "cpu/all/averageTemperature" = "Temperature moyenne";
+              };
+              SensorColors = {
+                "cpu/all/usage" = "14,0,209"; # Bleu
+              };
+              Sensors = {
+                highPrioritySensorIds = ''["cpu/all/usage"]'';
+                lowPrioritySensorIds = ''["cpu/cpu.*/usage","cpu/all/averageFrequency","cpu/all/averageTemperature"]'';
+                totalSensors = ''["cpu/all/usage"]'';
+              };
+            };
+          }
+          {
+            name = "org.kde.plasma.systemmonitor";
+            config = {
+              Appearance = {
+                title = "Mémoire";
+                chareFace = "org.kde.ksysguard.piechart";
+                updateRateLimit = "1000";
+              };
+              SensorLabels = {
+                "memory/physical/usedPercent" = "Usage %";
+                "memory/physical/used" = "Utilisation";
+                "memory/swap/used" = "Utilisation Swap";
+                "memory/swap/usedPercent" = "Usage Swap %";
+                "memory/physical/total" = "Total";
+                "memory/swap/total" = "Total Swap";
+              };
+              SensorColors = {
+                "memory/physical/usedPercent" = "0,209,14"; # Vert
+                "memory/swap/usedPercent" = "171,80,0"; # Orange
+              };
+              Sensors = {
+                highPrioritySensorIds = ''["memory/physical/usedPercent","memory/swap/usedPercent"]'';
+                lowPrioritySensorIds = ''["memory/physical/used","memory/physical/total","memory/swap/used","memory/swap/total"]'';
+                totalSensors = ''["memory/physical/usedPercent","memory/swap/usedPercent"]'';
+              };
+            };
+          }
+        ];
+      }
+      {
+        location = "top";
+        height = 26;
+        widgets = [
+          {
+            applicationTitleBar = {
+              behavior = {
+                activeTaskSource = "activeTask";
+                filterByActivity = true;
+                filterByScreen = true;
+                filterByVirtualDesktop = true;
+                disableForNotMaximized = false;
+                disableButtonsForNotHovered = false;
+              };
+              layout = {
+                elements = [ "windowIcon" "windowTitle" ];
+                horizontalAlignment = "left";
+                showDisabledElements = "deactivated";
+                verticalAlignment = "center";
+              };
+              overrideForMaximized.enable = false;
+              titleReplacements = [
+                {
+                  type = "regexp";
+                  originalTitle = ''\bDolphin\b'';
+                  newTitle = "File manager";
+                }
+                {
+                  type = "regexp";
+                  originalTitle = ''\bVisual Studio Code.*\b'';
+                  newTitle = "Visual Studio Code";
+                }
+                {
+                  type = "regexp";
+                  originalTitle = ''\bAntigravity.*\b'';
+                  newTitle = "Antigravity";
+                }
+              ];
+              windowTitle = {
+                undefinedWindowTitle = "";
+                font = {
+                  bold = false;
+                  fit = "fixedSize";
+                  size = 10;
+                };
+                hideEmptyTitle = true;
+                margins = {
+                  bottom = 0;
+                  left = 10;
+                  right = 5;
+                  top = 0;
+                };
+                source = "appName";
+              };
+            };
+          }
+          "org.kde.plasma.panelspacer"
+          {
+            digitalClock = {
+              calendar.firstDayOfWeek = "monday";
+              time.format = "24h";
+              time.showSeconds = "always";
+              date = {
+                enable = true;
+                position = "besideTime";
+                format = {
+                  custom = "ddd d/M/yyyy •";
+                };
+              };
+              font = null;
+            };
+          }
+          "org.kde.plasma.panelspacer"
+          {
+            plasmusicToolbar = {
+              panelIcon = {
+                albumCover = {
+                  useAsIcon = true;
+                  fallbackToIcon = true;
+                  radius = 8;
+                };
+                icon = "view-media-track";
+              };
+              playbackSource = "auto";
+              musicControls = {
+                showPlaybackControls = true;
+                volumeStep = 5;
+              };
+              songText = {
+                displayInSeparateLines = true;
+                maximumWidth = 400;
+                scrolling = {
+                  behavior = "alwaysScroll";
+                  speed = 3;
+                  resetOnPause = true;
+                };
+              };
+            };
+          }
+        ];
+      }
+    ];
+  };
+}
