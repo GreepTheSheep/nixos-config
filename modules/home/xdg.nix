946c0307041a29311a5c5a8e0b2ef130d1b7be2f
Matthieu <greep@greep.fr>
2025-12-18 16:00:37 +0100

    Added xdg portal

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -1,66 +1,75 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "x-scheme-handler/vscode" = "code.desktop";
-      "x-scheme-handler/antigravity" = "antigravity.desktop";
+  xdg = {
+    portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
     };
-  };
 
-  xdg.desktopEntries.code = {
-    name = "Visual Studio Code";
-    comment = "Code Editing. Redefined.";
-    genericName = "Text Editor";
-    exec = "${pkgs.writeShellScript "code-wrapper" ''
-      if echo "$1" | grep -q "^vscode:"; then
-        code --open-url "$@"
-      else
-        code "$@"
-      fi
-    ''} %U";
-    icon = "vscode";
-    startupNotify = true;
-    categories = [ "Utility" "TextEditor" "Development" "IDE" ];
-    mimeType = [ "text/plain" "inode/directory" "x-scheme-handler/vscode" ];
-    settings = {
-      StartupWMClass = "Code";
-      Keywords = "vscode";
-    };
-    actions = {
-      new-empty-window = {
-        name = "New Empty Window";
-        exec = "code --new-window %F";
-        icon = "vscode";
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "x-scheme-handler/vscode" = "code.desktop";
+        "x-scheme-handler/antigravity" = "antigravity.desktop";
       };
     };
-  };
 
-  xdg.desktopEntries.antigravity = {
-    name = "Antigravity";
-    comment = "Code Editing. Redefined.";
-    genericName = "Text Editor";
-    exec = "${pkgs.writeShellScript "antigravity-wrapper" ''
-      if echo "$1" | grep -q "^antigravity:"; then
-        antigravity --open-url "$@"
-      else
-        antigravity "$@"
-      fi
-    ''} %U";
-    icon = "antigravity";
-    startupNotify = true;
-    categories = [ "Utility" "TextEditor" "Development" "IDE" ];
-    mimeType = [ "text/plain" "inode/directory" "x-scheme-handler/antigravity" ];
-    settings = {
-      StartupWMClass = "Antigravity";
-      Keywords = "vscode";
-    };
-    actions = {
-      new-empty-window = {
-        name = "New Empty Window";
-        exec = "antigravity --new-window %F";
+    desktopEntries = {
+      code = {
+        name = "Visual Studio Code";
+        comment = "Code Editing. Redefined.";
+        genericName = "Text Editor";
+        exec = "${pkgs.writeShellScript "code-wrapper" ''
+          if echo "$1" | grep -q "^vscode:"; then
+            code --open-url "$@"
+          else
+            code "$@"
+          fi
+        ''} %U";
+        icon = "vscode";
+        startupNotify = true;
+        categories = [ "Utility" "TextEditor" "Development" "IDE" ];
+        mimeType = [ "text/plain" "inode/directory" "x-scheme-handler/vscode" ];
+        settings = {
+          StartupWMClass = "Code";
+          Keywords = "vscode";
+        };
+        actions = {
+          new-empty-window = {
+            name = "New Empty Window";
+            exec = "code --new-window %F";
+            icon = "vscode";
+          };
+        };
+      };
+
+      antigravity = {
+        name = "Antigravity";
+        comment = "Code Editing. Redefined.";
+        genericName = "Text Editor";
+        exec = "${pkgs.writeShellScript "antigravity-wrapper" ''
+          if echo "$1" | grep -q "^antigravity:"; then
+            antigravity --open-url "$@"
+          else
+            antigravity "$@"
+          fi
+        ''} %U";
         icon = "antigravity";
+        startupNotify = true;
+        categories = [ "Utility" "TextEditor" "Development" "IDE" ];
+        mimeType = [ "text/plain" "inode/directory" "x-scheme-handler/antigravity" ];
+        settings = {
+          StartupWMClass = "Antigravity";
+          Keywords = "vscode";
+        };
+        actions = {
+          new-empty-window = {
+            name = "New Empty Window";
+            exec = "antigravity --new-window %F";
+            icon = "antigravity";
+          };
+        };
       };
     };
   };
