369fcbf74bce8bee064bc8ab1d39729fc1543dde
Matthieu <greep@greep.fr>
2025-12-21 22:37:44 +0100

    Added sops templates

--- modules/davfs.nix
+++ modules/davfs.nix
@@ -4,15 +4,17 @@
   services.davfs2.enable = true;
   environment.systemPackages = [ pkgs.davfs2 ];
 
-  environment.etc."davfs2/secrets" = {
-    text = ''
-      https://cloud.greep.fr/remote.php/webdav/ greep ${config.sops.secrets."nextcloud/password"}
+  environment.etc."davfs2/secrets".source = config.sops.templates."davfs2-secrets".path;
+
+  sops.templates."davfs2-secrets" = {
+    content = ''
+      https://cloud.greep.fr/remote.php/webdav/ greep ${config.sops.placeholder."nextcloud/password"}
     '';
     mode = "0600";
   };
 
   fileSystems."/mnt/cloud" = {
-    device = "https://nextcloud.example.com/remote.php/webdav/";
+    device = "https://cloud.greep.fr/remote.php/webdav/";
     fsType = "davfs";
     options = [ "rw" "uid=1000" "gid=1000" "_netdev" "auto" ];
   };
