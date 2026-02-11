2a74835959bc961a9e234740a24ddb927f48685c
Matthieu <greep@greep.fr>
2026-02-11 16:21:24 +0100

    Added systemd service for backrest

--- modules/app-config/restic_backrest.nix
+++ modules/app-config/restic_backrest.nix
@@ -8,5 +8,19 @@
 
   # Backrest is accessed via web port 9898, we need to open it up using a firewall rule and environment variable
   networking.firewall.allowedTCPPorts = [ 9898 ];
-  environment.variables.BACKREST_PORT = "0.0.0.0:9898";
+  environment.variables= {
+    BACKREST_PORT = "0.0.0.0:9898";
+    BACKREST_RESTIC_COMMAND = "${pkgs.restic}/bin/restic";
+  };
+
+  # Create a systemd service to run backrest on startup
+  systemd.services.backrest = {
+    description = "Backrest Backup Service";
+    wantedBy = [ "multi-user.target" ];
+    path = with pkgs; [ backrest restic ];
+    serviceConfig = {
+      ExecStart = "${pkgs.backrest}/bin/backrest";
+      Restart = "always";
+    };
+  };
 }
