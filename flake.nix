784b5572b582e909ee27b13bb50650d2ff19980b
Matthieu <greep@greep.fr>
2026-02-14 13:05:41 +0100

    Revert changed nixpkgs branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Greep NixOS Configuration ISO";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
