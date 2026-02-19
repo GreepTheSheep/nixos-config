a612fbf5ebfcc2eb0ff01b01183776ea08f91ac1
Matthieu <greep@greep.fr>
2026-02-19 12:59:11 +0100

    Don't use davfs in live cd

--- modules/davfs.nix
+++ modules/davfs.nix
@@ -1,41 +1,46 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
+let
+  isLiveIso = config.networking.hostName == "greep-nixos-live-iso";
+in
 {
-  services.davfs2.enable = true;
-  environment.systemPackages = [ pkgs.davfs2 ];
+  config = lib.mkIf (!isLiveIso) {
+    services.davfs2.enable = true;
+    environment.systemPackages = [ pkgs.davfs2 ];
 
-  environment.etc."davfs2/secrets".source = config.sops.templates."davfs2-secrets".path;
+    environment.etc."davfs2/secrets".source = config.sops.templates."davfs2-secrets".path;
 
-  sops.templates."davfs2-secrets" = {
-    content = ''
-      https://cloud.greep.fr/remote.php/webdav/ greep ${config.sops.placeholder."nextcloud/password"}
-    '';
-    mode = "0600";
+    sops.templates."davfs2-secrets" = {
+      content = ''
+        https://cloud.greep.fr/remote.php/webdav/ greep ${config.sops.placeholder."nextcloud/password"}
+      '';
+      mode = "0600";
+    };
+
+    # Créer le point de montage
+    systemd.tmpfiles.rules = [
+      "d /mnt/cloud 0755 greep users -"
+    ];
+
+    # Unité de montage systemd qui se monte/démonte automatiquement avec le réseau
+    systemd.mounts = [{
+      description = "Nextcloud WebDAV Mount";
+      what = "https://cloud.greep.fr/remote.php/webdav/";
+      where = "/mnt/cloud";
+      type = "davfs";
+      options = "rw,uid=1000,gid=1000,_netdev,noauto,x-systemd.automount,x-systemd.idle-timeout=60";
+
+      # Monte uniquement quand le réseau est disponible
+      wantedBy = [ "remote-fs.target" ];
+      after = [ "network-online.target" ];
+      requires = [ "network-online.target" ];
+    }];
+
+    # Automount pour monter à la demande
+    systemd.automounts = [{
+      description = "Nextcloud WebDAV Automount";
+      where = "/mnt/cloud";
+      wantedBy = [ "multi-user.target" ];
+    }];
   };
-
-  # Créer le point de montage
-  systemd.tmpfiles.rules = [
-    "d /mnt/cloud 0755 greep users -"
-  ];
-
-  # Unité de montage systemd qui se monte/démonte automatiquement avec le réseau
-  systemd.mounts = [{
-    description = "Nextcloud WebDAV Mount";
-    what = "https://cloud.greep.fr/remote.php/webdav/";
-    where = "/mnt/cloud";
-    type = "davfs";
-    options = "rw,uid=1000,gid=1000,_netdev,noauto,x-systemd.automount,x-systemd.idle-timeout=60";
-
-    # Monte uniquement quand le réseau est disponible
-    wantedBy = [ "remote-fs.target" ];
-    after = [ "network-online.target" ];
-    requires = [ "network-online.target" ];
-  }];
-
-  # Automount pour monter à la demande
-  systemd.automounts = [{
-    description = "Nextcloud WebDAV Automount";
-    where = "/mnt/cloud";
-    wantedBy = [ "multi-user.target" ];
-  }];
 }
