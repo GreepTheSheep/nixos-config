16ad83142565b608fec64791dd750254082deb4f
Matthieu <greep@greep.fr>
2026-02-19 11:54:07 +0100

    Disable sops for live iso

--- hosts/live-iso.nix
+++ hosts/live-iso.nix
@@ -5,4 +5,12 @@
   networking.hostName = "greep-nixos-live-iso";
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  # Disable sops secrets for the live ISO (no GPG keys available)
+  sops = {
+    gnupg.home = lib.mkForce "";
+    gnupg.sshKeyPaths = lib.mkForce [];
+    secrets = lib.mkForce {};
+    templates = lib.mkForce {};
+  };
 }
