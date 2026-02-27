824e6a1c3d91ff467f890fc4a3bffd7e6015d635
Matthieu <greep@greep.fr>
2026-02-27 01:33:16 +0100

    Set mkForce on live-iso

--- hosts/live-iso.nix
+++ hosts/live-iso.nix
@@ -4,7 +4,7 @@
   system.stateVersion = "25.11";
   networking.hostName = "greep-nixos-live-iso";
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nixpkgs.hostPlatform = lib.mkForce "x86_64-linux";
 
   # Disable sops secrets for the live ISO (no SSH host keys available)
   sops = {
