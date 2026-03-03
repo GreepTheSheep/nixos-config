6e1730fc255c019f28a0381be3777d0f5400f4f0
Matthieu <greep@greep.fr>
2026-03-03 16:55:45 +0100

    Added zsh & p10k config file

--- homeModules/base/shell/zsh.nix
+++ homeModules/base/shell/zsh.nix
@@ -43,5 +43,14 @@
         "oxd" = "oxker";
       };
     };
+
+    home.file.".zshrc" = {
+      enable = true;
+      source = "${./zsh/.zshrc}";
+    };
+    home.file.".p10k.zsh" = {
+      enable = true;
+      source = "${./zsh/.p10k.zsh}";
+    };
   };
 }
