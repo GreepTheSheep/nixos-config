d9b00d8503b512c28f94293a4bead9cf3218d919
Matthieu <greep@greep.fr>
2026-03-04 11:04:42 +0100

    Disabled some applications to be lighter on the VM

--- hosts/pc-matt-nix-vm/home/home.nix
+++ hosts/pc-matt-nix-vm/home/home.nix
@@ -1,5 +1,21 @@
-_:
+{ lib, ... }:
 
 {
-  homeManager.applications.enable = true;
+  homeManager.applications = {
+    enable = true;
+
+    communication.enable = lib.mkForce false;
+    editing.enable = lib.mkForce false;
+    office.enable = lib.mkForce false;
+    sync.enable = lib.mkForce false;
+    screenshot.enable = lib.mkForce false;
+
+    media = {
+      audio.enable = lib.mkForce false;
+      jellyfin.enable = lib.mkForce false;
+      mpv.enable = lib.mkForce false;
+      obs-studio.enable = lib.mkForce false;
+      qbittorrent.enable = lib.mkForce false;
+    };
+  };
 }
