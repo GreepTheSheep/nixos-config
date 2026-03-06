369d69931fad8c286ee5f1ec6e6425eb2eda9342
Matthieu <greep@greep.fr>
2026-03-06 19:28:58 +0100

    Updated default key file

--- nixosModules/system/sops.nix
+++ nixosModules/system/sops.nix
@@ -35,7 +35,7 @@
       defaultSopsFile = ../../secrets/secrets.yaml;
       defaultSopsFormat = "yaml";
       age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-      age.keyFile = lib.mkDefault "/secrets/keys.txt";
+      age.keyFile = lib.mkDefault "/root/.secrets/keys.txt";
 
       secrets = {
         "nextcloud/password" = {};
