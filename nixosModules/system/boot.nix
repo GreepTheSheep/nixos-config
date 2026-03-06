f0a419e5e70af7449dc1fdbf1071d69ad6e8ebf4
Matthieu <greep@greep.fr>
2026-03-07 00:01:37 +0100

    Add auto num lock on boot

--- nixosModules/system/boot.nix
+++ nixosModules/system/boot.nix
@@ -36,5 +36,16 @@
     services.acpid.enable = true;
 
     services.udisks2.enable = true; # power managment events
+
+    # Enable num lock early on boot
+    boot.initrd.extraUtilsCommands = ''
+      copy_bin_and_libs ${pkgs.kbd}/bin/setleds
+    '';
+    boot.initrd.preDeviceCommands = ''
+      INITTY=/dev/tty[1-6]
+      for tty in $INITTY; do
+        /bin/setleds -D +num < $tty
+      done
+    '';
   };
 }
