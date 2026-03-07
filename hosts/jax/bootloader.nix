2bda076a8fc1946b55af8953a89422ab7781608d
Matthieu <greep@greep.fr>
2026-03-07 18:52:53 +0100

    Added Windows entry in Limine

--- hosts/jax/bootloader.nix
+++ hosts/jax/bootloader.nix
@@ -2,4 +2,10 @@ _:
 
 {
   boot.loader.limine.style.interface.resolution = "3840x1080";
+
+  nixos.system.bootloader.extraBootEntries = ''
+    /Windows
+      protocol: efi
+      path: boot():/EFI/Microsoft/Boot/bootmgfw.efi
+  '';
 }
