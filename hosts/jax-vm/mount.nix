6076f16b0195a6d240c77f7e23f68cdf6038cd36
Greep <greep@greep.fr>
2026-03-07 15:27:16 +0100

    Actualiser hosts/jax-vm/mount.nix

--- hosts/jax-vm/mount.nix
+++ hosts/jax-vm/mount.nix
@@ -29,6 +29,6 @@ _:
 
   # Hibernation — après install, récupérer l'offset avec:
   # sudo btrfs inspect-internal map-swapfile -r /swapfile
-  boot.resumeDevice = "/dev/disk/by-uuid/1153cd46-0b8c-4017-8383-fd99132dd4f5";
-  boot.kernelParams = [ "resume_offset=XXXXXXXX" ];
+  #boot.resumeDevice = "/dev/disk/by-uuid/1153cd46-0b8c-4017-8383-fd99132dd4f5";
+  #boot.kernelParams = [ "resume_offset=XXXXXXXX" ];
 }
