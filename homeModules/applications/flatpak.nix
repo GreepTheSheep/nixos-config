1a19410c622995b68a97b1d38a695ae8c23cf210
Matthieu <greep@greep.fr>
2026-03-08 21:26:38 +0100

    Added remote

--- homeModules/applications/flatpak.nix
+++ homeModules/applications/flatpak.nix
@@ -15,9 +15,9 @@
   config = lib.mkIf config.homeManager.applications.flatpak.enable {
     services.flatpak = {
       enable = true;
-      #remotes = {
-        #flathub = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-      #};
+      remotes = {
+        flathub = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+      };
       packages = [
         "com.github.tchx84.Flatseal"
         "it.mijorus.gearlever"
