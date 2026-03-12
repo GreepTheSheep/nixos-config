485bda7491d6b00934a560c84339174b403f5398
Matthieu <greep@greep.fr>
2026-03-12 09:13:38 +0100

    Updated disko for this host

--- hosts/jax-server-vm/disko.nix
+++ hosts/jax-server-vm/disko.nix
@@ -1,26 +1,4 @@
-{ disk ? "/dev/sda", luks ? false, ... }:
-
-let
-  btrfsContent = {
-    type = "btrfs";
-    extraArgs = [ "-L" "nixos" "-f" ];
-    subvolumes = {
-      "@" = {
-        mountpoint = "/";
-        mountOptions = [ "compress-force=zstd:2" "noatime" "space_cache=v2" ];
-      };
-      "@home" = {
-        mountpoint = "/home";
-        mountOptions = [ "compress-force=zstd:2" "noatime" "space_cache=v2" ];
-      };
-    };
-  };
-
-  # Quand luks=true, la partition est laissée brute : le script d'install
-  # gère cryptsetup manuellement (évite de télécharger tpm2-tss via la
-  # closure Nix de cryptsetup même sans TPM).
-  rootContent = if luks then null else btrfsContent;
-in
+{ disk ? "/dev/sda", ... }:
 
 {
   disko.devices = {
@@ -42,7 +20,13 @@ in
           };
           root = {
             size = "100%";
-          } // (if luks then {} else { content = rootContent; });
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-L" "nixos" "-f" ];
+              mountpoint = "/";
+              mountOptions = [ "noatime" ];
+            };
+          };
         };
       };
     };
