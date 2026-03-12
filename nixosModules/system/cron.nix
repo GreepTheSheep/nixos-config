dd5b88479d71d1fecefa275f1407c2d7fd41a3b6
Matthieu <greep@greep.fr>
2026-03-12 16:51:32 +0100

    Added systemCronJobs

--- nixosModules/system/cron.nix
+++ nixosModules/system/cron.nix
@@ -9,12 +9,25 @@
         example = true;
         description = "Enable cron service.";
       };
+
+      jobs = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [];
+        example = [
+          "0 * * * * crontab -l > /root/crontab-backup    # Note: this job is added automatically to the system in this cron.nix file"
+        ];
+        description = "A list of Cron jobs to be appended to the system-wide crontab.";
+      };
     };
   };
 
   config = lib.mkIf config.nixos.system.cron.enable {
     services.cron = {
       enable = true;
+
+      systemCronJobs = [
+        "0 * * * * crontab -l > /root/crontab-backup"
+      ] ++ config.nixos.system.cron.jobs;
     };
   };
 }
