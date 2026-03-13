a94afe035f4a0c48d4039631bef2cc9e78c046e0
Matthieu <greep@greep.fr>
2026-03-13 08:43:12 +0100

    Added timeout and extraConfig to Limine

--- nixosModules/system/bootloader.nix
+++ nixosModules/system/bootloader.nix
@@ -14,6 +14,16 @@
         default = "";
         description = "Entrées Limine supplémentaires par hôte (dual-boot, etc.).";
       };
+      timeout = lib.mkOption {
+        type = lib.types.int;
+        default = 5;
+        description = "Durée en secondes si pas d'input avant de lancer la première entrée. (une valeur a 0 va sauter le choix de l'entrée)";
+      };
+      extraConfig = lib.mkOption {
+        type = lib.types.lines;
+        default = "";
+        description = "Config Limine supplémentaires.";
+      };
     };
   };
 
@@ -30,6 +40,10 @@
       maxGenerations = 32;
       validateChecksums = true;
       panicOnChecksumMismatch = true;
+
+      extraConfig = ''
+        timeout: ${toString config.nixos.system.bootloader.timeout}
+      '' + config.nixos.system.bootloader.extraConfig;
 
       extraEntries = config.nixos.system.bootloader.extraBootEntries + ''
         /Reboot
