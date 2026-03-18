72eeddb20fcccbc3f8d8a29ee36f8b5a618a37e6
Matthieu <greep@greep.fr>
2026-03-18 22:00:18 +0100

    Removed SpotX and spotifywm,  replaced to default Spotify pkg

--- nixosModules/userEnvironment/spotify.nix
+++ nixosModules/userEnvironment/spotify.nix
@@ -39,19 +39,7 @@
     };
 
     environment.defaultPackages = with pkgs; [
-      spotifywm
+      spotify
     ];
-
-    systemd.services.spotx-setup = {
-      description = "Run SpotX installation";
-      after = [ "network-online.target" ];
-      wants = [ "network-online.target" ];
-      wantedBy = [ "multi-user.target" ];
-      path = with pkgs; [ curl bash perl zip unzip coreutils procps util-linux sudo ];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.curl}/bin/curl -sSL https://spotx-official.github.io/run.sh | ${pkgs.bash}/bin/bash -s -- -h'";
-      };
-    };
   };
 }
