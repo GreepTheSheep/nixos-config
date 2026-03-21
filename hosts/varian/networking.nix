a079464698f49493841c6244f0625a830b68e011
Matthieu <greep@greep.fr>
2026-03-21 02:03:11 +0100

    Added local ip for vigor and varian

--- hosts/varian/networking.nix
+++ hosts/varian/networking.nix
@@ -2,6 +2,11 @@ _:
 
 {
   networking = {
-    hostName = "varian";
+    hostName = "varian.greep.fr";
+  };
+
+  nixos.system.networking = {
+    enable = true;
+    localIP = "192.168.1.56";
   };
 }
--- hosts/vigor/networking.nix
+++ hosts/vigor/networking.nix
@@ -2,6 +2,11 @@ _:
 
 {
   networking = {
-    hostName = "vigor";
+    hostName = "vigor.greep.fr";
+  };
+
+  nixos.system.networking = {
+    enable = true;
+    localIP = "192.168.1.55";
   };
 }
