a7113e6a65865e7247d73a6826ecfd23ea771857
Matthieu <greep@greep.fr>
2026-04-04 22:51:16 +0200

    Updated network interface

--- hosts/vigor/networking.nix
+++ hosts/vigor/networking.nix
@@ -3,6 +3,22 @@ _:
 {
   networking = {
     hostName = "vigor";
+
+    interfaces = {
+      enp3s0 = {
+        wakeOnLan.enable = true;
+        ipv4.addresses = [
+          {
+            address = "192.168.1.55";
+            prefixLength = 24;
+          }
+        ];
+        ipv6.addresses = [];
+      };
+    };
+
+    defaultGateway.interface = "enp12s0";
+    defaultGateway6.interface = "enp12s0";
   };
 
   nixos.system.networking = {
