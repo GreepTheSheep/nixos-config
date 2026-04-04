1a6eadb43deedfbfe5031b3844f7359867cf9ca0
Matthieu <greep@greep.fr>
2026-04-05 00:04:45 +0200

    Move mounts to mount.nix

--- hosts/vigor/hardware-configuration.nix
+++ hosts/vigor/hardware-configuration.nix
@@ -13,44 +13,6 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/ee904141-f052-4f9b-907b-9a371fcf6617";
-      fsType = "btrfs";
-      options = [ "subvol=@" ];
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/5207-1CFF";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
-
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/ee904141-f052-4f9b-907b-9a371fcf6617";
-      fsType = "btrfs";
-      options = [ "subvol=@home" ];
-    };
-
-  fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/ee904141-f052-4f9b-907b-9a371fcf6617";
-      fsType = "btrfs";
-      options = [ "subvol=@nix" ];
-    };
-
-  fileSystems."/var/lib/docker" =
-    { device = "/dev/disk/by-uuid/ee904141-f052-4f9b-907b-9a371fcf6617";
-      fsType = "btrfs";
-      options = [ "subvol=@docker" ];
-    };
-
-  fileSystems."/var/log" =
-    { device = "/dev/disk/by-uuid/ee904141-f052-4f9b-907b-9a371fcf6617";
-      fsType = "btrfs";
-      options = [ "subvol=@log" ];
-    };
-
-  swapDevices = [ ];
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
