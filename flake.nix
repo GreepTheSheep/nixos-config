b5032e11ecc1f7fbe2ce347221d7faefd3315b24
Matthieu <greep@greep.fr>
2026-04-05 22:26:35 +0200

    Added nixpkgs-stable input

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     millennium = {
       url = "github:SteamClientHomebrew/Millennium?dir=packages/nix";
