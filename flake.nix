af2ce0df50bae96fe07172738ec83f74585a05ac
Matthieu <greep@greep.fr>
2026-04-05 22:47:38 +0200

    Added nix-colors

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nix-colors.url = "github:misterio77/nix-colors";
 
     millennium = {
       url = "github:SteamClientHomebrew/Millennium?dir=packages/nix";
