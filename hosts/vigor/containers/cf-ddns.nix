8bcdef20e90e559da1d1b3a9164804962c09eb0e
Matthieu <greep@greep.fr>
2026-04-06 16:06:31 +0200

    Reverted zone and added onlyOnChange

--- hosts/vigor/containers/cf-ddns.nix
+++ hosts/vigor/containers/cf-ddns.nix
@@ -29,92 +29,85 @@
               account_email = "${config.sops.placeholder."docker/cfddns/account-email"}";
             };
           };
-          zones = [
-            {
-              # A entries
-              id = "${config.sops.placeholder."docker/cfddns/zone-id"}";
-              name = "greep.fr";
-              subdomains = [
-                {
-                  name = "vigor";
-                  proxied = false;
-                  type = "A";
-                }
-                {
-                  name = "4.vigor";
-                  proxied = false;
-                  type = "A";
-                }
-                {
-                  name = "jellyfin";
-                  proxied = true;
-                  type = "A";
-                }
-                {
-                  name = "jellyfin-requests";
-                  proxied = true;
-                  type = "A";
-                }
-                {
-                  name = "cloud";
-                  proxied = true;
-                  type = "A";
-                }
-                {
-                  name = "immich";
-                  proxied = true;
-                  type = "A";
-                }
-                {
-                  name = "cdn";
-                  proxied = true;
-                  type = "A";
-                }
-              ];
-            }
-            {
-              # AAAA entries
-              id = "${config.sops.placeholder."docker/cfddns/zone-id"}";
-              name = "greep.fr";
-              subdomains = [
-                {
-                  name = "vigor";
-                  proxied = false;
-                  type = "AAAA";
-                }
-                {
-                  name = "6.vigor";
-                  proxied = false;
-                  type = "AAAA";
-                }
-                {
-                  name = "jellyfin";
-                  proxied = true;
-                  type = "AAAA";
-                }
-                {
-                  name = "jellyfin-requests";
-                  proxied = true;
-                  type = "AAAA";
-                }
-                {
-                  name = "cloud";
-                  proxied = true;
-                  type = "AAAA";
-                }
-                {
-                  name = "immich";
-                  proxied = true;
-                  type = "AAAA";
-                }
-                {
-                  name = "cdn";
-                  proxied = true;
-                  type = "AAAA";
-                }
-              ];
-            }
-          ];
+          zones = [{
+            id = "${config.sops.placeholder."docker/cfddns/zone-id"}";
+            name = "greep.fr";
+            subdomains = [
+              {
+                name = "vigor";
+                proxied = false;
+                type = "A";
+              }
+              {
+                name = "vigor";
+                proxied = false;
+                type = "AAAA";
+              }
+              {
+                name = "4.vigor";
+                proxied = false;
+                type = "A";
+              }
+              {
+                name = "6.vigor";
+                proxied = false;
+                type = "AAAA";
+              }
+              {
+                name = "jellyfin";
+                proxied = true;
+                type = "A";
+              }
+              {
+                name = "jellyfin";
+                proxied = true;
+                type = "AAAA";
+              }
+              {
+                name = "jellyfin-requests";
+                proxied = true;
+                type = "A";
+              }
+              {
+                name = "jellyfin-requests";
+                proxied = true;
+                type = "AAAA";
+              }
+              {
+                name = "cloud";
+                proxied = true;
+                type = "A";
+              }
+              {
+                name = "cloud";
+                proxied = true;
+                type = "AAAA";
+              }
+              {
+                name = "immich";
+                proxied = true;
+                type = "A";
+              }
+              {
+                name = "immich";
+                proxied = true;
+                type = "AAAA";
+              }
+              {
+                name = "cdn";
+                proxied = true;
+                type = "A";
+              }
+              {
+                name = "cdn";
+                proxied = true;
+                type = "AAAA";
+              }
+            ];
+          }];
+          updater = {
+            onlyOnChange = true;
+          };
         }];
       };
     };
