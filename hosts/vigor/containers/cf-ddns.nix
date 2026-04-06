66f17b04a74e2b9326ac4a8a383fa5035b710153
Matthieu <greep@greep.fr>
2026-04-06 15:09:29 +0200

    Updated cfddns config

--- hosts/vigor/containers/cf-ddns.nix
+++ hosts/vigor/containers/cf-ddns.nix
@@ -18,13 +18,31 @@
     directory = "${home}/docker-containers/cloudflare-ddns";
 
     cfddnsConfig = builtins.toJSON {
-      # TODO
+      accounts = [{
+        zones = [{
+          name = "greep.fr";
+          subdomains = [
+            {
+              name = "vigor";
+              proxied = false;
+              type = "A";
+            }
+            {
+              name = "vigor";
+              proxied = false;
+              type = "AAAA";
+            }
+          ];
+        }];
+      }];
     };
   in lib.mkIf config.host.containers.cfddns.enable {
     sops.secrets."docker/cfddns/api-token" = {};
+    sops.secrets."docker/cfddns/zone-id" = {};
 
     sops.templates."cfddns.env".content = ''
       CF_DDNS_API_TOKEN_1=${config.sops.placeholder."docker/cfddns/api-token"}
+      CF_DDNS_ZONE_ID_1=${config.sops.placeholder."docker/cfddns/zone-id"}
     '';
 
     systemd.tmpfiles.rules = [
