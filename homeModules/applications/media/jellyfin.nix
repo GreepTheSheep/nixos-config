217421570cb9ee741d585508fd001156135f1ab9
Matthieu <greep@greep.fr>
2026-04-11 12:36:59 +0200

    Added jellyfin-rpc

--- homeModules/applications/media/jellyfin.nix
+++ homeModules/applications/media/jellyfin.nix
@@ -9,12 +9,20 @@
         example = true;
         description = "Enable Jellyfin Desktop.";
       };
+
+      enableRPC = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable Jellyfin RPC, a module that displays the content you're currently watching on Discord. (Discord must be enabled to enable this)";
+      };
     };
   };
 
   config = lib.mkIf config.homeManager.applications.media.jellyfin.enable {
     home.packages = with pkgs; [
       jellyfin-desktop
+    ] ++ lib.optionals (config.homeManager.applications.media.jellyfin.enableRPC && config.homeManager.applications.communication.discord.enable) [
+      jellyfin-rpc
     ];
   };
 }
