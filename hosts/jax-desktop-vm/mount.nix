755aa2e23821dcf7766429caf1303612c193c377
Matthieu <greep@greep.fr>
2026-04-17 09:31:10 +0100

    Updated mount

--- hosts/jax-desktop-vm/mount.nix
+++ hosts/jax-desktop-vm/mount.nix
@@ -1,6 +1,37 @@
 { pkgs, config, lib, ... }:
 
 {
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/ad224100-79ad-4d83-8591-c74930470a6b";
+      fsType = "btrfs";
+      options = [ "subvol=@" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/ADED-F0C2";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/ad224100-79ad-4d83-8591-c74930470a6b";
+      fsType = "btrfs";
+      options = [ "subvol=@home" ];
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/ad224100-79ad-4d83-8591-c74930470a6b";
+      fsType = "btrfs";
+      options = [ "subvol=@nix" ];
+    };
+
+  fileSystems."/var/log" =
+    { device = "/dev/disk/by-uuid/ad224100-79ad-4d83-8591-c74930470a6b";
+      fsType = "btrfs";
+      options = [ "subvol=@log" ];
+    };
+
   swapDevices = [
     {
       device = "/swapfile";
