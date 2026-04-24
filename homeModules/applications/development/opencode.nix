2c1980252d6c96b842ca5f51e18db5596fc81263
Matthieu <greep@greep.fr>
2026-04-24 18:36:48 +0200

    Added OpenCode desktop app

--- homeModules/applications/development/opencode.nix
+++ homeModules/applications/development/opencode.nix
@@ -7,7 +7,14 @@
         type = lib.types.bool;
         default = false;
         example = true;
-        description = "Enable OpenCode.";
+        description = "Enable OpenCode Terminal.";
+      };
+
+      enableDesktop = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        example = false;
+        description = "Enable OpenCode Desktop. OpenCode Terminal must be enabled to enable the Desktop App.";
       };
     };
   };
@@ -16,5 +23,9 @@
     programs.opencode = {
       enable = true;
     };
+
+    home.packages = with pkgs; lib.mkIf config.homeManager.applications.development.opencode.enableDesktop [
+      opencode-desktop
+    ];
   };
 }
