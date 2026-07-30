e4be2fcdea2518f30c27b3d6a08061354e29238b
Matthieu <greep@greep.fr>
2026-07-30 20:20:23 +0200

    Fix path

--- homeModules/applications/wallpaperengine.nix
+++ homeModules/applications/wallpaperengine.nix
@@ -12,7 +12,7 @@
 
       assetsPath = lib.mkOption {
         type = lib.types.nullOr lib.types.str;
-        default = "~/.local/share/Steam/steamapps/common/wallpaper_engine/assets";
+        default = "${config.home.homeDirectory}/.local/share/Steam/steamapps/common/wallpaper_engine/assets";
         description = "Wallpaper Engine assets full path.";
       };
 
