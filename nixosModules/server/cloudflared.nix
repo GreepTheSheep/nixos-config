8c9d3985c5cec3e937a5e2709261843b6e7bb62d
Matthieu <greep@greep.fr>
2026-08-04 15:47:08 +0100

    Added cloudflared certificate

--- nixosModules/server/cloudflared.nix
+++ nixosModules/server/cloudflared.nix
@@ -13,8 +13,21 @@
   };
 
   config = lib.mkIf config.nixos.server.cloudflared.enable {
+    sops.secrets = {
+      "cloudflared/certificate" = {};
+    };
+
+    sops.templates = {
+      "cert.pem".content = ''
+        -----BEGIN ARGO TUNNEL TOKEN-----
+        ${config.sops.placeholder."cloudflared/certificate"}
+        -----END ARGO TUNNEL TOKEN-----
+      '';
+    };
+
     services.cloudflared = {
       enable = true;
+      certificateFile = config.sops.templates."cert.pem".path;
     };
   };
 }
