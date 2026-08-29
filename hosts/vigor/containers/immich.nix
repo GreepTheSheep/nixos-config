5dbacd5fe9d0f6df3de2f1c94adeb7705744c960
Matthieu <greep@greep.fr>
2026-08-29 16:47:38 +0100

    Update Immich postgres to 18

--- hosts/vigor/containers/immich.nix
+++ hosts/vigor/containers/immich.nix
@@ -135,7 +135,7 @@
       };
 
       "immich-postgres" = {
-        image = "ghcr.io/immich-app/postgres:14-vectorchord0.4.3-pgvectors0.2.0";
+        image = "ghcr.io/immich-app/postgres:18-vectorchord1.1.1-pgvector0.8.5";
         hostname = "immich_postgres";
         environmentFiles = [
           config.sops.templates."immich-postgres.env".path
