98ec1d8d20589301958829d7f460af3e5254ffda
Matthieu <greep@greep.fr>
2026-09-26 19:29:31 +0200

    Updated failing extraPackages and added replaceCat

--- nixosModules/base/shell/bat.nix
+++ nixosModules/base/shell/bat.nix
@@ -19,7 +19,7 @@
         type = lib.types.bool;
         default = true;
         example = false;
-        description = "Replace cat with bat.";
+        description = "Replace cat with bat. Cat will be accessible with cat-old";
       };
     };
   };
@@ -28,13 +28,22 @@
     programs.bat = {
       enable = true;
       extraPackages = with pkgs.bat-extras; [
-        core
         batdiff
         batman
         batgrep
         prettybat
         batwatch
       ];
+    };
+
+    programs.bash.shellAliases = lib.mkIf config.nixos.base.shell.bat.replaceCat {
+      cat-old = "${pkgs.coreutils}/bin/cat";
+      cat = "${pkgs.bat}/bin/bat";
+    };
+
+    programs.zsh.shellAliases = lib.mkIf config.nixos.base.shell.bat.replaceCat {
+      cat-old = "${pkgs.coreutils}/bin/cat";
+      cat = "${pkgs.bat}/bin/bat";
     };
   };
 }
